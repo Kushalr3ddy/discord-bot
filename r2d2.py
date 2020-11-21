87fb5a2117be23b231ddb7be3095908e4e36a45c
kushalr3ddy <pikkikushal@gmail.com>
2020-11-21 11:50:41 +0530

    smol update

--- r2d2.py
+++ r2d2.py
@@ -1,25 +1,28 @@
+
+from tkn import bot_token
 import discord
 from discord import message,Member
 from discord.ext import commands
+from discord.ext.commands import Greedy
 from random import randint,choice
 import keep_alive
-import wikia
+import wikipedia
 
-danbot_token = "Nzc3NzkzNjU3MzQwMjk3MjM2.X7Imyg.6NidwiHIDFMdMnP2tRTICC0UGJo"
-client = commands.Bot(command_prefix=">")
+cmd =""
+client = commands.Bot(command_prefix= ";")
 #####events
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.online,activity=discord.Game("krunker.io| >help"))
+    await client.change_presence(status=discord.Status.online,activity=discord.Game(";help"))
     print(f"logged in as {client.user}")
 
 @client.event
 async def on_message(ctx):
     if ctx.author == client.user:
         return
-    print(f"{ctx.author}:{ctx.content}")
+    #print(f"{ctx.author}:{ctx.content}")
     file1 = open("myfile.txt", "a")  # append mode 
-    file1.write(f"{ctx.author}:{ctx.content}") 
+    file1.write(f"{ctx.author}:{ctx.content}\n") 
     await client.process_commands(ctx)
 
 ###
@@ -38,7 +41,7 @@ async def joined(ctx,member:discord.Member):
 @client.command()
 async def repeat(ctx,message):
     """Repeats a message
-    usage: >repeat <your-message>"""
+    usage:\n;repeat <your-message>"""
     await ctx.send(message)
 
 @client.command()
@@ -53,25 +56,42 @@ async def flip(ctx):
     await ctx.send(f"{ctx.author} flipped {choice(coin)}")
 
 @client.command()
-async def yt(ctx):
+async def link(ctx):
     """Zoom link for classes"""
-    await ctx.send("https://zoom.us/j/95398188063?pwd=T3pOeWFlS1JxaG03WkNibHJVT3Fldz09")###
+    await ctx.send("https://zoom.us/j/95398188063?pwd=T3pOeWFlS1JxaG03WkNibHJVT3Fldz09")
+
+@client.command()
+async def conference(ctx):
+    await ctx.send("https://zoom.us/j/5516619991")
+###
+
 
 @client.command()
 @commands.has_permissions(manage_messages=True)
 async def clear(ctx,amount=1):
     """clears specified no of messages (default is 1)\n
-    usage: >clear <no-of-messages>"""
+    usage: ;clear <no-of-messages>"""
     await ctx.channel.purge(limit=amount+1)
 
 @client.command()
-async def nudge(ctx, users: discord.ext.commands.Greedy[discord.User]):
+async def nudge(ctx, users: Greedy[discord.User]):
+    """nudges the user mentioned in dm"""
     #user = await bot.get_user_info(user_id)
     #link = await ctx.channel.create_invite(max_age = 300)
     for user in users:
         await user.send(f"{ctx.author} nudged you")
         #await user.send(link)
 
+@client.command()
+async def  wiki(ctx,query,lines=2):
+    """does wikipedia search"""
+    try:
+        if len(query) == 0:
+            await ctx.send("usage ;wiki [search]")
+        else:
+            await ctx.send(wikipedia.summary(query,lines))
+    except:
+            await ctx.send("search not found or some error has occured")
 ######error_handling
 @client.event
 async def on_command_error(ctx,error):
@@ -80,7 +100,7 @@ async def on_command_error(ctx,error):
     elif isinstance(error,commands.MissingPermissions):
         await ctx.send("you're not allowed to do that")
     elif isinstance(error,commands.CommandNotFound):
-        await ctx.send("command not found\ntype >help for more info")
+        await ctx.send("command not found\ntype ;help for more info")
 
 
 
