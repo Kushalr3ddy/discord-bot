b155164c03b23718c5d7582cecb28939ce55d1ed
kushalr3ddy <pikkikushal@gmail.com>
2020-12-03 15:34:06 +0530

    .

--- HEADERS.py
+++
@@ -1,7 +0,0 @@
-headers = {
-    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5)",
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "accept-charset": "cp1254,ISO-8859-9,utf-8;q=0.7,*;q=0.3",
-    "accept-encoding": "gzip,deflate,sdch",
-    "accept-language": "tr,tr-TR,en-US,en;q=0.8",
-}
